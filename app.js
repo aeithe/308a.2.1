589e3a6af1285e2f3811e95f4b8916c85c79c1af
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-17 15:13:44 -0400

    finished as much as possible

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const adventurer = {
             inventory: ["small hat", "sunglasses"]
         }
     },
-    // roll (mod = 0) {
-    //     const result = Math.floor(Math.random() * 20) + 1 + mod;
-    //     console.log(`${this.name} rolled a ${result}.`)
-    //     }
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
     }
 
     for(i = 0; i< adventurer.inventory.length; i++){
@@ -31,18 +31,47 @@ const adventurer = {
           this.inventory = [];
           this.companion = {};
         }
-        rollDice() {
-            if(this.name = ""){
-             return   roll (mod = 0){
-                    let result = Math.floor(Math.random() * 20) + 1 + mod;
-                    console.log(`${this.name} rolled a ${result}.`)};
-                    }
-        };     }
+        roll (mod = 0) {
+            const result = Math.floor(Math.random() * 20) + 1 + mod;
+            console.log(`${this.name} rolled a ${result}.`)
+            }
+    };
+
 
       const robin = new Character("Robin");
+      robin.roll()
       robin.inventory = ["sword", "potion", "artifact"];
       robin.companion = new Character("Leo");
       robin.companion.type = "Cat";
       robin.companion.companion = new Character("Frank");
       robin.companion.companion.type = "Flea";
       robin.companion.companion.inventory = ["small hat", "sunglasses"];
+ 
+    
+class Adventurer extends Character {
+        constructor (name, attack, companion,role1, role2, role3) {
+          super(name,companion);
+          this.inventory.push("bedroll", "50 gold coins");
+          this.attack = attack;
+          this.role1 = role1;
+          this.role2 = role2;
+          this.role3 = role3;
+        }
+        scout () {
+          console.log(`${this.name} is scouting ahead...`);
+          super.roll();
+        }
+        static Max_Health(name){
+            return `${name} has a max health of 100.`;
+        }
+        static ROLES(fighter, healer, wizard){
+            
+        }
+
+    }
+    const alisa = new Adventurer("Alisa");
+    alisa.scout();
+    alisa.attack = "Fireball";
+
+    
+
